0dc62d47ad3eb77081e7ba6d4a6fdbe70a64df7b
David Papamichael <david.papamichael@retool.com>
2023-11-16 15:01:38 +0000

    Unprotecting resource Audit Log Explorer

--- apps/Admin/Audit Log Explorer/lib/filterApps.sql
+++
@@ -1,9 +0,0 @@
-select 
-  pages.name,
-  pages."updatedAt"
-from pages
-where  
-  pages.name ilike {{'%' + appFilter.inputValue + '%'}}
-  or pages.name = any({{appFilter.value}})
-order by pages."updatedAt" desc
-limit 100
